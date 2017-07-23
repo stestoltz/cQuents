228d35497cf07832f9c73375a523d0f5d7713ea3
Stephen Stoltzfus <stestoltz@comcast.net>
2017-07-23 02:00:53 -0400

    Linked sequences (WIP), OEIS functionality

--- cquents.py
+++ cquents.py
@@ -3,13 +3,31 @@ import math
 import sys
 
 LITERAL = "LITERAL"
+LITERAL_ESCAPE = "@"
+LITERAL_QUOTE = "'"
+
 NUMBER = "NUMBER"
-MODE = "MODE"
-ID = "ID"
-PARAM = "PARAM"
-BUILTIN = "BUILTIN"
-CONSTANT = "CONSTANT"
 EOF = "EOF"
+ID = "ID"
+
+BUILTIN = "BUILTIN"
+EXTRA_BUILTINS_START = "\\"
+OEIS_START = "O"
+
+CONSTANT = "CONSTANT"
+CONSTANT_START = "_"
+
+MODE = "MODE"
+SEQUENCE_1 = ":"
+SEQUENCE_2 = "::"
+SERIES = ";"
+QUERY = "?"
+
+PARAM = "PARAM"
+CURRENT = "$"
+START = "="
+DEFAULT_INPUT = "#"
+STRINGED = '"'
 
 PLUS = "+"
 MINUS = "-"
@@ -19,14 +37,15 @@ INT_DIV = "//"
 EXPONENT = "^"
 MOD = "%"
 
-# ~`!  # _{}[]?:;
-
-# &= | \"<>
-
 SEPARATOR = ","
 META_SEPARATOR = "|"
 LPAREN = "("
 RPAREN = ")"
+NEWLINE = "\n"
+
+# ~`!  # _{}[]?:;
+
+# &= | \"<>
 
 is_one_int = re.compile("^[0-9]$")
 is_id = re.compile("^[$v-zA-E]$")
@@ -84,6 +103,9 @@ class Builtins:
             math.log2(inter.visit(node.parameters[0]))
         return math.log(inter.visit(node.parameters[0]), base)
 
+    def get_line(self, node, lines_, index):
+        return lines_[index].interpreter.interpret(node.parameters)
+
 
 builtin_helper = Builtins()
 
@@ -98,8 +120,25 @@ builtins = {
     "\\c": lambda inter, node: math.cos(inter.visit(node.parameters[0])),
     "\\l": lambda inter, node: math.log10(inter.visit(node.parameters[0])),
     "\\s": lambda inter, node: math.sin(inter.visit(node.parameters[0])),
-    "\\t": lambda inter, node: math.tan(inter.visit(node.parameters[0]))
+    "\\t": lambda inter, node: math.tan(inter.visit(node.parameters[0])),
+    # functions for other lines
+    "\\0": lambda inter, node: builtin_helper.get_line(node, lines, 0),
+    "\\1": lambda inter, node: builtin_helper.get_line(node, lines, 1),
+    "\\2": lambda inter, node: builtin_helper.get_line(node, lines, 2),
+    "\\3": lambda inter, node: builtin_helper.get_line(node, lines, 3),
+    "\\4": lambda inter, node: builtin_helper.get_line(node, lines, 4),
+    "\\5": lambda inter, node: builtin_helper.get_line(node, lines, 5),
+    "\\6": lambda inter, node: builtin_helper.get_line(node, lines, 6),
+    "\\7": lambda inter, node: builtin_helper.get_line(node, lines, 7),
+    "\\8": lambda inter, node: builtin_helper.get_line(node, lines, 8),
+    "\\9": lambda inter, node: builtin_helper.get_line(node, lines, 9)
 }
+
+OEIS = {
+    "A000001": lambda inter, node: 1
+}
+
+builtins.update(OEIS)
 
 constants = {
     "e": math.e,
@@ -154,21 +193,13 @@ class Sequence:
 
         # TODO: work with not just ints
         if self.node.is_stringed:
-            return int(interpreter.join.join([str(x) for x in self.sequence])[self.current - 1])
+            return int(self.interpreter.join.join([str(x) for x in self.sequence])[self.current - 1])
 
         return cur_val
 
 
 def get_input_val(char):
     return ord(char) - 65
-
-
-def get_test_lexer(text):
-    test_lexer = Lexer(text)
-    token = test_lexer.read_token()
-    while token.type != EOF:
-        token = test_lexer.read_token()
-    return test_lexer
 
 
 class Token:
@@ -194,38 +225,36 @@ class Lexer:
         self.param_found = False
         self.mode = None
 
-        self.max_input = -1
-
     def read_token(self):
         if self.cur is None:
             return Token(EOF, "")
 
         if not self.mode:
-            if self.cur in (":", ";", "?"):
+            if self.cur in (SEQUENCE_1, SERIES, QUERY):
                 self.mode = self.cur
                 self.param_found = True
                 self.advance()
 
-                if self.mode == self.cur == ":":
-                    self.mode += ":"
+                if self.mode == self.cur == SEQUENCE_1:
+                    self.mode += SEQUENCE_1
                     self.advance()
 
                 return Token(MODE, self.mode)
-            elif self.cur in ("=", "#", "$", '"'):
+            elif self.cur in (START, DEFAULT_INPUT, CURRENT, STRINGED):
                 self.param_found = True
                 param = self.cur
                 self.advance()
                 return Token(PARAM, param)
-            elif self.cur == "|":
+            elif self.cur == META_SEPARATOR:
                 self.advance()
-                return Token(META_SEPARATOR, "|")
+                return Token(META_SEPARATOR, META_SEPARATOR)
 
         if self.param_found:
             if self.cur == "." or is_one_int.match(self.cur):
                 return self.read_number()
             elif is_id.match(self.cur):
                 return self.read_id()
-            elif self.cur == "_":
+            elif self.cur == CONSTANT_START:
                 self.advance()
                 temp = self.cur
                 self.advance()
@@ -234,10 +263,16 @@ class Lexer:
                 temp = self.cur
                 self.advance()
                 return Token(BUILTIN, temp)
-            elif self.cur == "\\" and self.cur + self.peek() in builtins:
+            elif self.cur == EXTRA_BUILTINS_START and self.cur + self.peek() in builtins:
                 temp = self.cur
                 self.advance()
                 temp += self.cur
+                self.advance()
+                return Token(BUILTIN, temp)
+            elif self.cur == OEIS_START:
+                self.advance()
+                temp = str(self.read_number().val)
+                temp = self.cur + "0" * (6 - len(temp)) + temp
                 self.advance()
                 return Token(BUILTIN, temp)
             elif self.cur in (PLUS, MINUS, MUL, DIV, EXPONENT, MOD, LPAREN, RPAREN, SEPARATOR):
@@ -250,23 +285,26 @@ class Lexer:
                 self.advance()
                 return Token(temp, temp)
 
-        if self.cur == "@":
+        if self.cur == LITERAL_ESCAPE:
             self.advance()
             temp = self.cur
             self.advance()
             return Token(LITERAL, temp)
-        elif self.cur == "'":
+        elif self.cur == LITERAL_QUOTE:
             self.advance()
             temp = self.read_literal()
             self.advance()
             return temp
+        elif self.cur == NEWLINE:
+            self.advance()
+            return Token(NEWLINE, NEWLINE)
         else:
             if self.cur is not None:
                 temp = self.cur
                 self.advance()
                 return Token(LITERAL, temp)
 
-        raise SyntaxError("Unknown character found : " + self.cur)
+        # raise SyntaxError("Unknown character found : " + self.cur)
 
     def advance(self):
         self.pos += 1
@@ -278,10 +316,6 @@ class Lexer:
     def read_id(self):
         temp = self.cur
         self.advance()
-        if is_input_id.match(temp):
-            code = get_input_val(temp)
-            if self.max_input < code:
-                self.max_input = code
         return Token(ID, temp)
 
     def read_number(self):
@@ -309,7 +343,7 @@ class Lexer:
     def read_literal(self):
         result = ""
 
-        while self.cur is not None and self.cur != "'":
+        while self.cur is not None and self.cur != LITERAL_QUOTE:
             result += self.cur
             self.advance()
 
@@ -326,7 +360,6 @@ class Parser:
     def __init__(self, lexer_):
         self.lexer = lexer_
         self.token = self.lexer.read_token()
-        self.test_lexer = get_test_lexer(self.lexer.text)
 
         self.operations_1 = (EXPONENT,)
         self.operations_2 = (MUL, DIV, INT_DIV, MOD)
@@ -336,48 +369,27 @@ class Parser:
         if self.token.type == type_:
             self.token = self.lexer.read_token()
         else:
-            if type_ == RPAREN or type_ == EOF:
+            if type_ == EOF or type_ == RPAREN and self.token.type in (EOF, NEWLINE):
                 return
 
             raise SyntaxError("Incorrect token found: looking for " + type_ + ", found " + self.token.type + " | " + self.token.val + " at " + self.lexer.pos)
 
-    def parse(self, input_):
-        program = self.program(input_)
-        self.eat(EOF)
-        return program
+    def parse(self):
+        lines_ = [self.program()]
+        while self.token.type == NEWLINE:
+            self.eat(NEWLINE)
+            lines_.append(self.program())
 
-    def program(self, input_):
+        self.eat(EOF)
+        return lines_
+
+    def program(self):
         params = self.params()
         mode = self.mode()
         items = self.items()
 
-        default_input_length = len(params.default_input[0]) + len(params.default_input[1])
-        user_input_length = len(input_)
-
-        actual_input_length = default_input_length + user_input_length
-        expected_input_length = self.test_lexer.max_input + 1
-
-        # print("Actual: " + str(actual_input_length) + ", Expected: " + str(expected_input_length))
-
-        input_length_difference = actual_input_length - expected_input_length
-
-        if input_length_difference == 0 or input_length_difference == 1:
-            all_input = []
-
-            all_input.extend(params.default_input[0])
-            all_input.extend(input_)
-            all_input.extend(params.default_input[1])
-
-            n = None
-            if input_length_difference == 1:
-                n = all_input.pop()
-
-            # print(all_input)
-
-            program = Program(params, all_input, n, mode, items)
-            return program
-
-        raise ValueError("Incorrect input length")
+        program = Program(params, mode, items)
+        return program
 
     def params(self):
         literals = ["", "", ""]
@@ -398,16 +410,16 @@ class Parser:
         is_stringed = False
 
         while self.token.type == PARAM:
-            if self.token.val == "#":
+            if self.token.val == DEFAULT_INPUT:
                 self.eat(PARAM)
                 default_input = self.input_list()
-            elif self.token.val == "=":
+            elif self.token.val == START:
                 self.eat(PARAM)
                 start = self.items()
-            elif self.token.val == "$":
+            elif self.token.val == CURRENT:
                 self.eat(PARAM)
                 current_start = self.items()
-            elif self.token.val == '"':
+            elif self.token.val == STRINGED:
                 self.eat(PARAM)
                 is_stringed = True
 
@@ -541,17 +553,88 @@ class NodeVisitor:
         raise ValueError("No visit_" + type(node).__name__ + " method")
 
 
+class Tester(NodeVisitor):
+    def __init__(self):
+        self.max_input = -1
+        self.mode = None
+
+    def generic_visit(self, node):
+        pass
+
+    def visit_Program(self, node):
+        self.mode = node.mode
+
+        for each in node.current_start:
+            self.visit(each)
+        for each in node.start:
+            self.visit(each)
+        for each in node.input_front:
+            self.visit(each)
+        for each in node.input_back:
+            self.visit(each)
+        for each in node.statements:
+            self.visit(each)
+
+    def visit_BinOp(self, node):
+        self.visit(node.left)
+        self.visit(node.right)
+
+    def visit_Builtin(self, node):
+        for parameter in node.parameters:
+            self.visit(parameter)
+
+    def visit_UnaryOp(self, node):
+        if node.op.type in unary_ops:
+            self.visit(node.expr)
+
+    def visit_Var(self, node):
+        if is_input_id.match(node.name):
+            code = get_input_val(node.name)
+            if self.max_input < code:
+                self.max_input = code
+
+
 class Interpreter(NodeVisitor):
-    def __init__(self, tree_):
+    def __init__(self, tree_, max_input):
         self.tree = tree_
+        self.input = None
+        self.max_input = max_input
+
         self.sequence = None
         self.current = 1
         self.current_inc = 1
         self.program = None
-        self.join = ","
+        self.join = SEPARATOR
 
     def visit_Program(self, node):
-        # TODO: Do different if mode == "?"
+        default_input_length = len(node.input_front) + len(node.input_back)
+        user_input_length = len(self.input)
+
+        actual_input_length = default_input_length + user_input_length
+        expected_input_length = self.max_input + 1
+
+        # print("Actual: " + str(actual_input_length) + ", Expected: " + str(expected_input_length))
+
+        input_length_difference = actual_input_length - expected_input_length
+
+        if input_length_difference == 0 or input_length_difference == 1:
+            all_input = []
+
+            all_input.extend(node.input_front)
+            all_input.extend(self.input)
+            all_input.extend(node.input_back)
+
+            n = None
+            if input_length_difference == 1:
+                n = all_input.pop()
+
+            self.input = all_input
+
+            # print(self.input)
+        else:
+            raise ValueError("Incorrect input length")
+
+        # TODO: Do different if mode == QUERY
 
         self.program = node
         self.sequence = Sequence(self, node)
@@ -561,19 +644,18 @@ class Interpreter(NodeVisitor):
         # starting literals
         print(node.literals[0], end="")
 
-        if node.n is not None:
-            n = self.visit(node.n)
-            query_n = n == 0 or n
-        else:
-            n = None
+        if n is None:
             query_n = False
+        else:
+            n = self.visit(n)
+            query_n = n == 0 or n
 
         if node.is_stringed:
             self.join = node.literals[1] or ""
         else:
             self.join = node.literals[1] or SEPARATOR
 
-        if node.mode in ("::", "?") and not query_n:
+        if node.mode in (SEQUENCE_2, QUERY) and not query_n:
             pass
 
         else:
@@ -581,7 +663,7 @@ class Interpreter(NodeVisitor):
 
             for val in self.sequence:
 
-                if node.mode == ":":
+                if node.mode == SEQUENCE_1:
                     if query_n:
                         if n == self.current:
                             print(val, end="")
@@ -589,7 +671,7 @@ class Interpreter(NodeVisitor):
                     else:
                         print(val, end=self.join)
 
-                elif node.mode == "::":
+                elif node.mode == SEQUENCE_2:
                     if query_n:
                         if n == self.current:
                             print(val, end="")
@@ -597,7 +679,7 @@ class Interpreter(NodeVisitor):
                         else:
                             print(val, end=self.join)
 
-                elif node.mode == ";":
+                elif node.mode == SERIES:
                     sum_ += val
 
                     if query_n:
@@ -606,7 +688,7 @@ class Interpreter(NodeVisitor):
                             break
                     # elif sum_ == previous_sum
 
-                elif node.mode == "?":
+                elif node.mode == QUERY:
                     if query_n:
                         if n == val:
                             print("true", end="")
@@ -643,7 +725,7 @@ class Interpreter(NodeVisitor):
             return unary_ops[node.op.type](self.visit(node.expr))
 
     def visit_Var(self, node):
-        if node.name == "$":
+        if node.name == CURRENT:
             return self.current
         elif is_previous_id.match(node.name):
             try:
@@ -652,15 +734,22 @@ class Interpreter(NodeVisitor):
                 temp = 0
             return temp
         elif is_input_id.match(node.name):
-            return self.visit(self.program.input[get_input_val(node.name)])
+            return self.visit(self.input[get_input_val(node.name)])
 
     def visit_Number(self, node):
         return node.value
 
-    def interpret(self):
+    def interpret(self, input_):
+        self.input = input_
         if self.tree is None:
             return None
         return self.visit(self.tree)
+
+
+class Line:
+    def __init__(self, tree, interpreter):
+        self.tree = tree
+        self.interpreter = interpreter
 
 
 class Params:
@@ -677,12 +766,12 @@ class AST:
 
 
 class Program(AST):
-    def __init__(self, params, input_, n, mode, statements):
+    def __init__(self, params, mode, statements):
         self.literals = params.literals
-        self.input = input_
+        self.input_front = params.default_input[0]
+        self.input_back = params.default_input[1]
         self.start = params.start
         self.current_start = params.current_start
-        self.n = n
         self.mode = mode
         self.statements = statements
         self.is_stringed = params.is_stringed
@@ -734,34 +823,56 @@ source = open(file).read()
 
 # print(source)
 
-try:
-    user_input = input().split(" ")
-except EOFError:
-    user_input = []
 
-if user_input == ['']:
-    user_input = []
+def get_input():
+    try:
+        input_ = input().split(" ")
+    except EOFError:
+        input_ = []
 
-if user_input is not []:
-    for input_index in range(len(user_input)):
-        item = user_input[input_index]
-        try:
-            user_input[input_index] = Number(Token(NUMBER, int(item)))
-        except ValueError:
+    if input_ == ['']:
+        input_ = []
+
+    if input_ is not []:
+        for input_index in range(len(input_)):
+            item = input_[input_index]
             try:
-                user_input[input_index] = Number(Token(NUMBER, float(item)))
+                input_[input_index] = Number(Token(NUMBER, int(item)))
             except ValueError:
-                user_input[input_index] = Number(Token(LITERAL, item))
+                try:
+                    input_[input_index] = Number(Token(NUMBER, float(item)))
+                except ValueError:
+                    input_[input_index] = Number(Token(LITERAL, item))
+
+    return input_
+
+user_input = get_input()
 
 # print(user_input)
 
-lexer = Lexer(source)
-parser = Parser(lexer)
+lines = []
 
-if parser.test_lexer.mode is None:
-    lexer = Lexer(":" + source)
+
+def run(cq_source, cq_input):
+    lexer = Lexer(cq_source)
     parser = Parser(lexer)
+    programs = parser.parse()
 
-tree = parser.parse(user_input)
-interpreter = Interpreter(tree)
-interpreter.interpret()
+    first = True
+    for program in programs:
+        tester = Tester()
+        tester.visit(program)
+
+        if first:
+            first = False
+            if tester.mode is None:
+                run(":" + cq_source, cq_input)
+                return
+
+        interpreter = Interpreter(program, tester.max_input)
+
+        lines.append(Line(program, interpreter))
+
+    lines[0].interpreter.interpret(cq_input)
+
+run(source, user_input)
